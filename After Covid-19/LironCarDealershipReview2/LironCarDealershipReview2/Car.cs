10ab7e2a49a41dbddebfc773133bea6bd16b1535
Liron Saedi <Liron.Saedi@GMR.local>
2020-07-15 17:29:47 -0700

    I finished the class of the Car Dealership.  Now I am just writing out all of the Console.Writilines

--- After Covid-19/LironCarDealershipReview2/LironCarDealershipReview2/Car.cs
+++ After Covid-19/LironCarDealershipReview2/LironCarDealershipReview2/Car.cs
@@ -6,5 +6,45 @@ namespace LironCarDealershipReview2
 {
     class Car
     {
+        string name;
+        int price;
+        int year;
+        string model;
+        string color;
+
+        public Car(string name, int price, int year, string model, string color)
+        {
+            this.name = name;
+            this.price = price;
+            this.year = year;
+            this.model = model;
+            this.color = color;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public int GetPrice()
+        {
+            return price;
+        }
+
+        public int GetYear()
+        {
+            return year;
+        }
+
+        public string GetModel()
+        {
+            return model;
+        }
+
+        public string GetColor()
+        {
+            return color;
+        }
+
     }
 }
--- After Covid-19/LironCarDealershipReview2/LironCarDealershipReview2/Program.cs
+++ After Covid-19/LironCarDealershipReview2/LironCarDealershipReview2/Program.cs
@@ -1,7 +1,23 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
 
 namespace LironCarDealershipReview2
 {
+    class test
+    {
+        int something { get; set; }
+        public test(int something, string temp, int price)
+        {
+            this.something = something;
+        }
+
+        public int GetSomething()
+        {
+            return something;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -9,11 +25,27 @@ namespace LironCarDealershipReview2
             Console.WriteLine("Hello World!");
 
             //Car class
-            //Price, Year, Rims, Model, Color
+            //Price, Year, Model, Color
             //Get Functions for them (No need for set functions)
 
             //List of the Car class containing x amount of cars
-            int money = 0;
+            int money = 200000;
+
+            List<Car> list = new List<Car>();
+
+            list.Add(new Car("Honda", 15000, 1998, "Civic", "Gray"));
+            list.Add(new Car("Toyota", 18000, 2003, "Camary", "Matte Black"));
+            list.Add(new Car("Mercedes-Benz", 90000, 2019, "Gls-450", "Peral White"));
+            list.Add(new Car("BMW", 78000, 2020, "X6", "Red"));
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i].GetName());
+            }
+            Console.WriteLine("Which Car Would You Like To View More Information On?");
+            int userNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{list[userNum]}");
         }
     }
 }
